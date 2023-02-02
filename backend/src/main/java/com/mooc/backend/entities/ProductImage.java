f3d1715e1395725870288a4ca266cf5c93e57926
Peng <wpcfan@gmail.com>
2023-02-02 12:14:30 +0800

    convert to lombok

--- backend/src/main/java/com/mooc/backend/entities/ProductImage.java
+++ backend/src/main/java/com/mooc/backend/entities/ProductImage.java
@@ -1,7 +1,15 @@
 package com.mooc.backend.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "mooc_product_images")
 public class ProductImage extends Auditable {
@@ -17,28 +25,28 @@ public class ProductImage extends Auditable {
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
-    public Long getId() {
-        return id;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ProductImage other = (ProductImage) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
 }

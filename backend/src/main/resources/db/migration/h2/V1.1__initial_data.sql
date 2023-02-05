17d210410d385b80afc8b75650a43d42f1708ba7
Peng <wpcfan@gmail.com>
2023-02-05 08:11:43 +0800

    change image url

--- backend/src/main/resources/db/migration/h2/V1.1__initial_data.sql
+++ backend/src/main/resources/db/migration/h2/V1.1__initial_data.sql
@@ -127,50 +127,50 @@ values (23, 12);
 insert into mooc_product_categories (`product_id`, `category_id`)
 values (24, 12);
 insert into mooc_product_images (`product_id`, `image_url`)
-values (1, 'https://www.edx.org/sites/default/files/course/image/promoted/business-analytics-1.jpg');
+values (1, 'https://images.pexels.com/photos/12951893/pexels-photo-12951893.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (2, 'https://www.edx.org/sites/default/files/course/image/promoted/ai-for-everyone-1.jpg');
+values (2, 'https://images.pexels.com/photos/9928345/pexels-photo-9928345.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (3, 'https://www.edx.org/sites/default/files/course/image/promoted/ai-for-everyone-1.jpg');
+values (3, 'https://images.pexels.com/photos/8622885/pexels-photo-8622885.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (4, 'https://www.edx.org/sites/default/files/course/image/promoted/data-science-1.jpg');
+values (4, 'https://images.pexels.com/photos/11700840/pexels-photo-11700840.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (5, 'https://www.edx.org/sites/default/files/course/image/promoted/data-science-1.jpg');
+values (5, 'https://images.pexels.com/photos/8042737/pexels-photo-8042737.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (6, 'https://www.edx.org/sites/default/files/course/image/promoted/data-science-1.jpg');
+values (6, 'https://images.pexels.com/photos/4659444/pexels-photo-4659444.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (7, 'https://www.edx.org/sites/default/files/course/image/promoted/design-thinking-1.jpg');
+values (7, 'https://images.pexels.com/photos/7426478/pexels-photo-7426478.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (8, 'https://www.edx.org/sites/default/files/course/image/promoted/healthcare-analytics-1.jpg');
+values (8, 'https://images.pexels.com/photos/7541732/pexels-photo-7541732.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (9, 'https://www.edx.org/sites/default/files/course/image/promoted/healthcare-analytics-1.jpg');
+values (9, 'https://images.pexels.com/photos/9143960/pexels-photo-9143960.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (10, 'https://www.edx.org/sites/default/files/course/image/promoted/learn-spanish-1.jpg');
+values (10, 'https://images.pexels.com/photos/9143958/pexels-photo-9143958.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (11, 'https://www.edx.org/sites/default/files/course/image/promoted/learn-spanish-1.jpg');
+values (11, 'https://images.pexels.com/photos/9143964/pexels-photo-9143964.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (12, 'https://www.edx.org/sites/default/files/course/image/promoted/learn-spanish-1.jpg');
+values (12, 'https://images.pexels.com/photos/9143965/pexels-photo-9143965.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (13, 'https://www.edx.org/sites/default/files/course/image/promoted/mathematical-thinking-1.jpg');
+values (13, 'https://images.pexels.com/photos/9144030/pexels-photo-9144030.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (14, 'https://www.edx.org/sites/default/files/course/image/promoted/mathematical-thinking-1.jpg');
+values (14, 'https://images.pexels.com/photos/11681061/pexels-photo-11681061.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (15, 'https://www.edx.org/sites/default/files/course/image/promoted/leadership-1.jpg');
+values (15, 'https://images.pexels.com/photos/4045619/pexels-photo-4045619.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (16, 'https://www.edx.org/sites/default/files/course/image/promoted/robotics-1.jpg');
+values (16, 'https://images.pexels.com/photos/10661108/pexels-photo-10661108.jpeg?auto=compress&cs=tinysrgb&w=1600');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (17, 'https://www.edx.org/sites/default/files/course/image/promoted/psychology-1.jpg');
+values (17, 'https://images.pexels.com/photos/13911326/pexels-photo-13911326.jpeg?auto=compress&cs=tinysrgb&w=1600');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (18, 'https://www.edx.org/sites/default/files/course/image/promoted/ielts-1.jpg');
+values (18, 'https://images.pexels.com/photos/9588261/pexels-photo-9588261.jpeg?auto=compress&cs=tinysrgb&w=1600');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (19, 'https://www.edx.org/sites/default/files/course/image/promoted/ielts-1.jpg');
+values (19, 'https://images.pexels.com/photos/10566125/pexels-photo-10566125.jpeg?auto=compress&cs=tinysrgb&w=1600');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (20, 'https://www.edx.org/sites/default/files/course/image/promoted/ielts-1.jpg');
+values (20, 'https://images.pexels.com/photos/4045031/pexels-photo-4045031.jpeg?auto=compress&cs=tinysrgb&w=1600');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (21, 'https://www.edx.org/sites/default/files/course/image/promoted/none-1.jpg');
+values (21, 'https://images.pexels.com/photos/5390114/pexels-photo-5390114.jpeg');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (22, 'https://www.edx.org/sites/default/files/course/image/promoted/none-1.jpg');
+values (22, 'https://images.pexels.com/photos/6625403/pexels-photo-6625403.jpeg');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (23, 'https://www.edx.org/sites/default/files/course/image/promoted/none-1.jpg');
+values (23, 'https://images.pexels.com/photos/12528815/pexels-photo-12528815.jpeg');
 insert into mooc_product_images (`product_id`, `image_url`)
-values (24, 'https://www.edx.org/sites/default/files/course/image/promoted/none-1.jpg');
+values (24, 'https://images.pexels.com/photos/7034219/pexels-photo-7034219.jpeg');

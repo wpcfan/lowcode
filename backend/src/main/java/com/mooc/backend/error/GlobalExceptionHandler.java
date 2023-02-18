ed59e993a45ba20584362a24b382194ccb4acf8a
Peng <wpcfan@gmail.com>
2023-02-18 22:54:20 +0800

    fix GlobalExceptionHandler

--- backend/src/main/java/com/mooc/backend/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/mooc/backend/error/GlobalExceptionHandler.java
@@ -20,12 +20,18 @@ public class GlobalExceptionHandler {
 
     private final MessageSource messageSource;
 
+    // 从配置文件中读取 hostname
     @Value("${hostname}")
     private String hostname;
 
+    /**
+     * 处理自定义异常
+     * @param ex 异常
+     * @param request 请求
+     * @return 错误信息
+     */
     @ExceptionHandler(CustomException.class)
     public ProblemDetail handleRecordNotFoundException(CustomException ex, WebRequest request) {
-
         ProblemDetail body = ProblemDetail
                 .forStatusAndDetail(HttpStatusCode.valueOf(500), ex.getLocalizedMessage());
         body.setType(URI.create(hostname + "/errors/" + ex.getCode()));
@@ -38,23 +44,35 @@ public class GlobalExceptionHandler {
         return body;
     }
 
+    /**
+     * 处理使用 jakarta.validation 注解的参数校验异常，
+     * @param e 异常
+     * @param request 请求
+     * @return 错误信息
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e, WebRequest request) {
         ProblemDetail body = ProblemDetail
                 .forStatusAndDetail(HttpStatusCode.valueOf(400), e.getLocalizedMessage());
 
         body.setType(URI.create(hostname + "/errors/validation"));
-        body.setTitle(e.getMessage());
-        String details = e.getBindingResult().getFieldErrors().stream()
+        String title = e.getBindingResult().getFieldErrors().stream()
                 .map(fieldError -> fieldError.getField() + " " + fieldError.getDefaultMessage())
                 .collect(Collectors.joining(", "));
-        body.setDetail(details);
+        body.setTitle(title);
+        body.setDetail(e.getMessage());
         body.setProperty("hostname", hostname);
         body.setProperty("ua", Optional.ofNullable(request.getHeader("User-Agent")).orElse("Unknown"));
         body.setProperty("locale", request.getLocale().toString());
         return body;
     }
 
+    /**
+     * 处理未捕获的异常
+     * @param ex 异常
+     * @param request 请求
+     * @return 错误信息
+     */
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleException(Exception ex, WebRequest request) {
         ProblemDetail body = ProblemDetail

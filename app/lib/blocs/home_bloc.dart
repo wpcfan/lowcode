91b375e878da852d94602118ca137aad2e14f5e3
Peng <wpcfan@gmail.com>
2023-02-20 11:56:44 +0800

    update

--- app/lib/blocs/home_bloc.dart
+++ app/lib/blocs/home_bloc.dart
@@ -6,16 +6,27 @@ import 'helpers.dart';
 import 'home_event.dart';
 import 'home_state.dart';
 
+/// HomeBloc
+/// 主页的业务逻辑
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final PageRepository pageRepo;
   final ProductRepository productRepo;
   HomeBloc({required this.pageRepo, required this.productRepo})
       : super(HomeState.initial()) {
+    /// 首页内容加载
     on<HomeEventFetch>(_onFetchHome,
         transformer: throttleDroppable(throttleDuration));
+
+    /// 瀑布流加载更多
     on<HomeEventLoadMore>(_onLoadMore);
+
+    /// 切换底部导航
     on<HomeEventSwitchBottomNavigation>(_onSwitchBottomNavigation);
+
+    /// 打开抽屉
     on<HomeEventOpenDrawer>(_onOpenDrawer);
+
+    /// 关闭抽屉
     on<HomeEventCloseDrawer>(_onCloseDrawer);
   }
 
@@ -37,20 +48,30 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   void _onFetchHome(HomeEvent event, Emitter<HomeState> emit) async {
     emit(HomeState.loading());
     try {
+      /// 获取首页布局
       final layout = await pageRepo.getByPageType(event.pageType);
+
+      /// 如果没有布局，返回错误
       if (layout.blocks.isEmpty) {
         emit(HomeState.error());
         return;
       }
+
+      /// 如果有瀑布流布局，获取瀑布流数据
       if (layout.blocks
           .where((element) => element.type == PageBlockType.waterfall)
           .isNotEmpty) {
+        /// 获取第一个瀑布流布局
         final waterfallBlock = layout.blocks.firstWhere(
                 (element) => element.type == PageBlockType.waterfall)
             as WaterfallPageBlock;
+
+        /// 如果瀑布流布局有内容，获取瀑布流数据
         if (waterfallBlock.data.isNotEmpty) {
+          /// 获取瀑布流数据的分类ID
           final categoryId = waterfallBlock.data.first.content.id;
           if (categoryId != null) {
+            /// 按分类获取瀑布流中的产品数据
             final waterfall =
                 await productRepo.getByCategory(categoryId: categoryId);
             emit(HomeState.populated(layout,

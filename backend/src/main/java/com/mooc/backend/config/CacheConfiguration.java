b6ac437b5dd0d4859b3eb46e0db03901c3c1c485
Peng <wpcfan@gmail.com>
2023-03-05 13:05:27 +0800

    add cache

--- backend/src/main/java/com/mooc/backend/config/CacheConfiguration.java
+++ backend/src/main/java/com/mooc/backend/config/CacheConfiguration.java
@@ -9,11 +9,13 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Profile("prod")
 @EnableCaching
 @Configuration
 public class CacheConfiguration {

d4209de7033f9e5bc23f866cbd4d56f1a69e4d52
Peng Wang <wpcfan@gmail.com>
2023-03-08 16:24:47 +0800

    add futurebuilder to fetch the category data

--- admin/lib/views/page/category_data_form.dart
+++ admin/lib/views/page/category_data_form.dart
@@ -1,5 +1,7 @@
 import 'package:admin/views/page/category_tree.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:models/models.dart';
 
 class CategoryDataForm extends StatelessWidget {
@@ -7,36 +9,37 @@ class CategoryDataForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final client = context.read<Dio>();
     return SingleChildScrollView(
-      child: CategoryTree(
-          categories: const [
-            Category(name: 'Food', children: [
-              Category(name: 'Bread'),
-              Category(name: 'Vegetables', children: [
-                Category(name: 'Tomatoes'),
-                Category(name: 'Potatoes'),
-              ]),
-              Category(name: 'Fruits', children: [
-                Category(name: 'Apples'),
-                Category(name: 'Bananas'),
-                Category(name: 'Oranges'),
-              ]),
-            ]),
-            Category(name: 'Clothing', children: [
-              Category(name: 'Shirts'),
-              Category(name: 'Pants'),
-              Category(name: 'Shoes'),
-            ]),
-            Category(name: 'Electronics', children: [
-              Category(name: 'Computers'),
-              Category(name: 'Smartphones'),
-              Category(name: 'Tablets'),
-            ]),
-          ],
-          onSelectionChanged: (categories) {
-            for (var category in categories) {
-              debugPrint(category.name ?? '');
+      child: FutureBuilder(
+          future: client.get('/categories'),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return const Center(child: CircularProgressIndicator());
             }
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            final res = snapshot.data;
+            if (res == null || res.data == null || res.data is! List) {
+              return const Text('没有分类数据');
+            }
+
+            /// 从响应数据中解析出分类列表
+            /// 由于响应数据中的分类数据是 Map<String, dynamic> 类型的
+            /// 所以需要将其转换为 Category 类型
+            /// 注意 List<dynamic> 不能直接转换为 List<Category>
+            /// 所以需要使用 List.from() 方法
+            final categories = List<Category>.from(res.data
+                .map((e) => Category.fromJson(e as Map<String, dynamic>)));
+
+            return CategoryTree(
+                categories: categories,
+                onSelectionChanged: (categories) {
+                  for (var category in categories) {
+                    debugPrint(category.name ?? '');
+                  }
+                });
           }),
     );
   }

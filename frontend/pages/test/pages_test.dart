b785fe6824082cc1dfeeeb6f73863a000796b784
Peng <wpcfan@gmail.com>
2023-03-20 10:42:38 +0800

    update tests

--- frontend/pages/test/pages_test.dart
+++ frontend/pages/test/pages_test.dart
@@ -1,5 +1,50 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:models/models.dart';
+import 'package:pages/blocs/blocs.dart';
+import 'package:repositories/repositories.dart';
+
+class MockPageAdminRepository extends Mock implements PageAdminRepository {}
 
 void main() {
-  test('adds one to input values', () {});
+  late PageAdminRepository adminRepo;
+  late PageBloc pageBloc;
+  setUp(() {
+    adminRepo = MockPageAdminRepository();
+    pageBloc = PageBloc(adminRepo);
+
+    when(() => adminRepo.search(any<PageQuery>()))
+        .thenAnswer((_) async => PageWrapper(
+              items: [],
+              page: 0,
+              size: 10,
+              totalPage: 0,
+              totalSize: 0,
+            ));
+  });
+
+  setUpAll(() {
+    registerFallbackValue(const PageQuery());
+  });
+
+  tearDown(() {
+    pageBloc.close();
+  });
+
+  // Test for PageEventTitleChanged
+  blocTest<PageBloc, PageState>(
+    'emits updated state when title changed',
+    build: () => pageBloc,
+    act: (bloc) => bloc.add(PageEventTitleChanged('New Title')),
+    expect: () => [
+      const PageState(
+        status: FetchStatus.loading,
+      ),
+      const PageState(
+        query: PageQuery(title: 'New Title'),
+        status: FetchStatus.populated,
+      )
+    ], // Add expected states here
+  );
 }
--- frontend/repositories/test/page_repository_test.dart
+++ frontend/repositories/test/page_repository_test.dart
@@ -1,5 +1,88 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:models/models.dart';
+import 'package:repositories/category_repository.dart';
+
+class MockDio extends Mock implements Dio {}
 
 void main() {
-  test('adds one to input values', () {});
+  group('CategoryRepository', () {
+    late Dio client;
+    late CategoryRepository repository;
+
+    setUp(() {
+      client = MockDio();
+      repository = CategoryRepository(client: client);
+    });
+
+    test('getCategories - basic', () async {
+      final categories = [
+        const Category(id: 1, name: 'Category 1'),
+        const Category(id: 2, name: 'Category 2'),
+      ];
+
+      when(client.get('/categories',
+              queryParameters: anyNamed('queryParameters')))
+          .thenAnswer((_) async => Response(
+              data: categories,
+              requestOptions: RequestOptions(
+                path: '/categories',
+              )));
+
+      final result = await repository.getCategories(
+          categoryRepresenation: CategoryRepresenation.basic);
+
+      expect(result, categories);
+    });
+
+    test('getCategories - withChildren', () async {
+      final categories = [
+        const Category(
+          id: 1,
+          name: 'Category 1',
+          children: [
+            Category(id: 2, name: 'Category 2'),
+            Category(id: 3, name: 'Category 3'),
+          ],
+        ),
+        const Category(
+          id: 4,
+          name: 'Category 4',
+          children: [
+            Category(id: 5, name: 'Category 5'),
+          ],
+        ),
+      ];
+
+      when(client.get('/categories',
+              queryParameters: anyNamed('queryParameters')))
+          .thenAnswer((_) async => Response(
+              data: categories,
+              requestOptions: RequestOptions(path: '/categories')));
+
+      final result = await repository.getCategories(
+          categoryRepresenation: CategoryRepresenation.withChildren);
+
+      expect(result, categories);
+    });
+
+    test('getCategories - rootOnly', () async {
+      final categories = [
+        const Category(id: 1, name: 'Category 1'),
+        const Category(id: 2, name: 'Category 2'),
+      ];
+
+      when(client.get('/categories',
+              queryParameters: anyNamed('queryParameters')))
+          .thenAnswer((_) async => Response(
+              data: categories,
+              requestOptions: RequestOptions(path: '/categories')));
+
+      final result = await repository.getCategories(
+          categoryRepresenation: CategoryRepresenation.rootOnly);
+
+      expect(result, categories);
+    });
+  });
 }

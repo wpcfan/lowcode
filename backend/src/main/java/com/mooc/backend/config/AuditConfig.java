341175923a4ac4b03e2ae8c8bad881e00a69814e
Peng Wang <wpcfan@gmail.com>
2023-03-22 11:41:56 +0800

    add comments

--- backend/src/main/java/com/mooc/backend/config/AuditConfig.java
+++ backend/src/main/java/com/mooc/backend/config/AuditConfig.java
@@ -3,6 +3,9 @@ package com.mooc.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * JPA 审计配置
+ */
 @EnableJpaAuditing
 @Configuration
 public class AuditConfig {
